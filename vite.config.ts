a15ba41717de687b6ea61a5e5ecd80a4cbb6ad7c
esouqu <nippl3juic3@gmail.com>
2024-05-31 02:56:12 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/Rick-and-Morty/" : "/",
   plugins: [
     vue(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+    },
+  },
 })
